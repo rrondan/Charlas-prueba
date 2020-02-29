59ff2d154cfa5571a5dcfaf3e1d12dbd027773fe
rrondan <rondanrichard@gmail.com>
2020-02-29 15:40:06 -0500

    funcionalidad de mapa para el detalle de la charla.

--- app/src/main/java/com/cibertec/pruebacharlas/MainActivity.java
+++ app/src/main/java/com/cibertec/pruebacharlas/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int numero = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for(int i = 0 ; i < 1000; i++){
+            numero++;
+        }
     }
 }
