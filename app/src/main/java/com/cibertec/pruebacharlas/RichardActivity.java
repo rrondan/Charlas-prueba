b3e500e6c3419b98391c5afe92edbfc8474cd192
rrondan <rondanrichard@gmail.com>
2020-02-29 15:53:19 -0500

    agregando richard activity

--- app/src/main/java/com/cibertec/pruebacharlas/RichardActivity.java
+++ app/src/main/java/com/cibertec/pruebacharlas/RichardActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    int numero = 1;
+public class RichardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_richard);
     }
 }
