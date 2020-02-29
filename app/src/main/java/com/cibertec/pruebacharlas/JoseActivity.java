705b5f98cf6fa57031c0147a232d85e78ab9d879
Jose <jlbautistavelasquez@gmail.com>
2020-02-29 16:28:28 -0500

    Cambios 16:28

--- app/src/main/java/com/cibertec/pruebacharlas/JoseActivity.java
+++ app/src/main/java/com/cibertec/pruebacharlas/JoseActivity.java
@@ -10,5 +10,7 @@ public class JoseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jose);
+
+        //mensaje joseb
     }
 }
