480e8fcb8f089f9e83b5ec0b1080c4ef39089f49
luansn <luansn1705@gmail.com>
2020-02-29 15:49:30 -0500

    Cración de un activity luansn

---
+++ app/src/main/java/com/cibertec/pruebacharlas/LuansnActivity.java
@@ -0,0 +1,16 @@
+package com.cibertec.pruebacharlas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LuansnActivity extends AppCompatActivity {
+
+    int dato=1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luansn);
+        dato++;
+    }
+}
